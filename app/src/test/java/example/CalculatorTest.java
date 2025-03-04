55a41612cce14a5ad382d81470f9bfce5c812da7
haomfeng <haomin.feng@student.unimelb.edu.au>
2025-03-04 17:09:09 +1100

    workshop 0: divide by zero test

--- app/src/test/java/example/CalculatorTest.java
+++ app/src/test/java/example/CalculatorTest.java
@@ -5,24 +5,35 @@ import org.junit.Test;
 
 public class CalculatorTest {
     Calculator calculator;
+
     @Before
     public void setup() {
         calculator = new Calculator();
     }
+
     @Test
     public void testAddBasic() {
         Assert.assertEquals(4, calculator.add(1, 3));
     }
+
     @Test
     public void testMinusBasic() {
         Assert.assertEquals(10, calculator.minus(15, 5));
     }
+
     @Test
     public void testMultiplyBasic() {
         Assert.assertEquals(10, calculator.multiply(2, 5));
     }
+
     @Test
     public void testDivideBasic() {
         Assert.assertEquals(5, calculator.divide(10, 2), 0.5);
+    }
+
+    @Test
+    public void testDivideByZero() {
+        RuntimeException exception = Assert.assertThrows(RuntimeException.class, () -> calculator.divide(10, 0));
+        Assert.assertEquals("Divide by zero", exception.getMessage());
     }
 }
